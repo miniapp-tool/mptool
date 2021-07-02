8b3244cb3f887c978452221b896d5e90fce4334e
Mr.Hope <zhangbowang1998@gmail.com>
2021-07-02 20:58:36 +0800

    chore: update demo

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -1,14 +1,15 @@
-import { $App } from "@mptool/enhance";
+import { $App, $Config } from "@mptool/enhance";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 wx.env.DEBUG = true;
 
+$Config({
+  routes: ["test/pages/$page", "/pages/$page"],
+  getRoute: (name: string) => `/pages/${name}`,
+});
+
 $App({
-  config: {
-    routes: ["test/pages/$page", "/pages/$page"],
-    getRoute: (name: string) => `/pages/${name}`,
-  },
   onLaunch(opts) {
     console.log("APP launch:", opts);
     this.$emitter.on("message to app", (msg: string) => {
