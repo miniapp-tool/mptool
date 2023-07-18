7481886dc08c5c60f474fe6f19b01d83bb80dc0b
Mr.Hope <mister-hope@outlook.com>
2023-07-18 21:42:08 +0800

    feat: add buffer2base64 helper

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,6 +1,9 @@
-/** 日志管理器 */
+export {
+  encode as encodeBase64,
+  decode as decodeBase64,
+} from "base64-arraybuffer";
+export * from "./utils.js";
 export * from "./emitter.js";
 export * as logger from "./logger.js";
 export * as query from "./query.js";
 export * from "./type.js";
-export * from "./utils.js";
