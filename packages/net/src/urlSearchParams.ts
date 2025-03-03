62d364814c2d1236b95a285e31e8486fd9a80df1
Mr.Hope <mister-hope@outlook.com>
2025-03-03 16:49:03 +0800

    chore: tweaks

--- packages/net/src/urlSearchParams.ts
+++ packages/net/src/urlSearchParams.ts
@@ -1,4 +1,29 @@
-const replace: Record<string, string> = {
+/**
+ * Forked from https://github.com/jerrybendy/url-search-params-polyfill
+ *
+ * MIT License
+ *
+ * Copyright (c) 2016 Jerry Bendy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+const ENCODE_MAP: Record<string, string> = {
   // eslint-disable-next-line @typescript-eslint/naming-convention
   "!": "%21",
   // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -18,7 +43,7 @@ const replace: Record<string, string> = {
 const encode = (str: string): string =>
   encodeURIComponent(str).replace(
     /[!'()~]|%20|%00/g,
-    (match) => replace[match],
+    (match) => ENCODE_MAP[match],
   );
 
 const decode = (str: string): string =>
