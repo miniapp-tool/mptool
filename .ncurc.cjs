c73b5df1191c4c1cc98ca7200137d2508bcbe86d
Mr.Hope <mister-hope@outlook.com>
2026-03-23 11:12:09 +0800

    chore: tweaks

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -5,7 +5,9 @@ module.exports = defineConfig({
   peer: true,
   upgrade: true,
   timeout: 360000,
+  workspaces: true,
   target: (name) => {
+    if (name === "vuepress" || name.startsWith("@vuepress/")) return "@next";
     if (name === "@types/node") return "minor";
 
     return "latest";
