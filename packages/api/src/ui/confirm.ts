688d4e404b9e629fc53b91e6f983c7cd45e1e12e
Mr.Hope <mister-hope@outlook.com>
2026-04-14 11:22:16 +0800

    fix: fix confirm api

--- packages/api/src/ui/confirm.ts
+++ packages/api/src/ui/confirm.ts
@@ -5,14 +5,14 @@ import { showModal } from "./modal.js";
  *
  * @param text 行为文字
  * @param confirmAction 确定回调函数
- * @param cancelAction 取消回调函数
  * @param warning 额外警告信息
+ * @param cancelAction 取消回调函数
  */
 export const confirm = (
   text: string,
   confirmAction: () => Promise<void> | void,
-  cancelAction: () => void | Promise<void> = (): void => void 0,
   warning = "",
+  cancelAction: () => void | Promise<void> = (): void => void 0,
 ): void => {
   showModal("操作确认", `您确定要${text}么?${warning}`, confirmAction, cancelAction);
 };
